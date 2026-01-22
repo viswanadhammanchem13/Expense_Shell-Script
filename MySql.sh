dc4d613765a35be320436f30e9fc80dc7182a1c3
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2026-01-22 11:03:34 +0530

    Expense

--- MySql.sh
+++ MySql.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_Folder="/var/log/Roboshop"
+LOGS_Folder="/var/log/Expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
 mkdir -p $LOGS_Folder
